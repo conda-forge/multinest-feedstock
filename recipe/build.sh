8875c68e8c14061cd7b50a7ffa71458173e7aab9
Colm Talbot <talbotcolm@gmail.com>
2024-06-13 11:39:57 -0500

    attempt simplifying macos build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-if [ "$(uname)" == "Darwin" ]; then
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DLAPACK_LIBRARIES=${PREFIX}/lib/liblapack.so -DBLAS_LIBRARIES=${PREFIX}/lib/libcblas.so ..
+make
+make install
+
+# if [ "$(uname)" == "Darwin" ]; then
     
-    cd build
-    cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DLAPACK_LIBRARIES=${PREFIX}/lib/liblapack.dylib -DBLAS_LIBRARIES=${PREFIX}/lib/libcblas.dylib \
-          -DCMAKE_C_COMPILER=clang \
-          -DCMAKE_C_FLAGS=${CFLAGS} \
-          -DCMAKE_CXX_COMPILER=clang++ \
-          -DCMAKE_CXX_FLAGS=${CXXFLAGS} \
-          -DCMAKE_CXX_STANDARD=98 \
-          ..
-    make
-    make install
+#     cd build
+#     cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DLAPACK_LIBRARIES=${PREFIX}/lib/liblapack.dylib -DBLAS_LIBRARIES=${PREFIX}/lib/libcblas.dylib \
+#           -DCMAKE_C_COMPILER=clang \
+#           -DCMAKE_C_FLAGS=${CFLAGS} \
+#           -DCMAKE_CXX_COMPILER=clang++ \
+#           -DCMAKE_CXX_FLAGS=${CXXFLAGS} \
+#           -DCMAKE_CXX_STANDARD=98 \
+#           ..
+#     make
+#     make install
     
-fi
+# fi
 
-if [ "$(uname)" == "Linux" ]; then
+# if [ "$(uname)" == "Linux" ]; then
 
-    cd build
-    cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DLAPACK_LIBRARIES=${PREFIX}/lib/liblapack.so -DBLAS_LIBRARIES=${PREFIX}/lib/libcblas.so ..
-    make
-    make install
+#     cd build
+#     cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DLAPACK_LIBRARIES=${PREFIX}/lib/liblapack.so -DBLAS_LIBRARIES=${PREFIX}/lib/libcblas.so ..
+#     make
+#     make install
 
-fi
+# fi
