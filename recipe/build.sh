4b53dd31b94b971abd7c12c1feec6a0af9bee874
Nick Wogan <nicholaswogan@gmail.com>
2025-08-12 08:34:50 -0700

    print statements

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+echo "BUILD STARTS HERE"
 
 if [ "$(uname)" == "Darwin" ]; then
-    
+    echo "BUILD TYPE is Darwin"
+
     cd build
     cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DLAPACK_LIBRARIES=${PREFIX}/lib/liblapack.dylib -DBLAS_LIBRARIES=${PREFIX}/lib/libcblas.dylib \
           -DCMAKE_C_COMPILER=clang \
@@ -17,6 +19,8 @@ if [ "$(uname)" == "Darwin" ]; then
 fi
 
 if [ "$(uname)" == "Linux" ]; then
+
+    echo "BUILD TYPE is Linux"
 
     cd build
     cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DLAPACK_LIBRARIES=${PREFIX}/lib/liblapack.so -DBLAS_LIBRARIES=${PREFIX}/lib/libcblas.so -DCMAKE_Fortran_FLAGS="-std=legacy" ..
